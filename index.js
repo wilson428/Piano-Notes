594985503bd20d5928c3a654491492f74a3163fc
Chris Wilson <christopher.e.wilson>
2021-01-01 23:29:31 -0500

    Can reference notes directly from  instance

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Notes } from './lib/Notes';
+import { Piano } from './lib/Audio/Audio';
 import { Piano_500 } from './lib/Audio/Audio_500';
 import { Piano_1000 } from './lib/Audio/Audio_1000';
 import { Piano_2000 } from './lib/Audio/Audio_2000';
 
-export { Notes, Piano_500, Piano_1000, Piano_2000 }
+export { Notes, Piano, Piano_500, Piano_1000, Piano_2000 }
